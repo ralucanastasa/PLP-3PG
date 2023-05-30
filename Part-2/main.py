8f5dc7d44253b8ca5bef068ca163be80637d5f60
ralucanastasa <ralucanastasa3@gmail.com>
2023-05-30 09:55:18 +0300

    part 2 of assessment added

---
+++ Part-2/main.py
@@ -0,0 +1,28 @@
+"""
+1. Create a syntax error of your own by entering your favorite icecream flavor in interactive mode. Then, make up for
+your misdeed and enter a statement that prints the name of your favorite ice cream.
+"""
+
+
+# fav_icecream_flavor = hazelnut
+fav_icecream_flavor = "salted caramel"
+print("My favorite icecream flavor is: ", fav_icecream_flavor)
+
+
+"""
+2. Write and save a program that prints out your name and waits for the user to press the Enter key before the program ends.
+"""
+
+my_name = "Raluca"
+print("My name is: ", my_name)
+input("--press enter to exit--")
+
+
+"""
+3. Write a program that prints your favorite quote. It should give credit to the person who said it on the next line. 
+"""
+
+the_quote = "It is never too late to be what you might have been."
+the_man_who_said_it = "George Eliot"
+print(the_quote)
+print("-said by", the_man_who_said_it)
